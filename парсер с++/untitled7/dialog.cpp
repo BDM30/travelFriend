6d9c09870fdb2f8a201b2b05de0e3ab0f88f88c7
Vladislaff Evtushenko <starson4587@gmail.com>
2014-07-16 12:06:01 +0400

    парсится Node
    -идем вперед

--- парсер с++/untitled7/dialog.cpp
+++ парсер с++/untitled7/dialog.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int const amount = 1000;
+int const amount = 15000;
 
 class Node {
 public:
@@ -21,6 +21,27 @@ public:
     double lat;
     double lon;
 };
+
+class Way {
+public:
+    void setNode(QString input) {
+        nodes[amountNodes] = input;
+        amountNodes++;
+    }
+    void setId(QString input) {
+        id = input;
+    }
+    Way()
+    {
+        amountNodes = 0;
+        //QString *nodes = new QString[amount]();
+    }
+    QString id;
+    QString nodes[amount];
+    //QString *nodes;
+    int amountNodes;
+};
+
 
 
 Dialog::Dialog(QWidget *parent) :
@@ -54,8 +75,15 @@ void Dialog::slotReadyRead()
 
 void Dialog::parseXml()
 {
-    Node arrayNodes[amount];
-    int curId = 0;
+    //Node arrayNodes[amount];
+    Node *arrayNodes = new Node[amount]();
+    int amountNodes = 0;
+
+    long counter = 0; // следит за открытием\закрытием тега way
+    long amountWays = 0;
+    //Way arrayWays[amount];
+    Way *arrayWays = new Way[amount]();
+
     while (!xmlReader.atEnd())
         {
          xmlReader.readNext();
@@ -67,12 +95,36 @@ void Dialog::parseXml()
              double three = attrib.value("lon").toDouble();
              if (one != "")
              {
-                arrayNodes[curId].set(one,two,three);
-                curId++;
+                arrayNodes[amountNodes].set(one,two,three);
+                amountNodes++;
+             }
+         }
+         if (token == "way") {
+                      if (counter % 2 == 0) { // тег way открылся
+                          QString one = attrib.value("id").toString();
+                          arrayWays[amountWays].setId(one);
+                      }
+                      else { // тег way закрылся
+                          amountWays++;
+                      }
+                      ui->textEdit->append("\nway\n");
+                      counter++;
+          }
+         if (counter % 2 == 1 && token == "nd") {
+             QString one = attrib.value("ref").toString();\
+             if (one != "") {
+                arrayWays[amountWays].setNode(one);
              }
          }
         }
-    for (int i = 0; i < curId; i++) {
+    // тестовый вывод
+    for (int i = 0; i < amountNodes; i++) {
         ui->textEdit->append(arrayNodes[i].id+ " " + QString::number(arrayNodes[i].lat) + " " + QString::number(arrayNodes[i].lon));
+    }
+    for (int i = 0; i < amountWays; i++ ) {
+            ui->textEdit->append("\n" + arrayWays[i].id + "\n");
+        }
+    for (int i = 0; i < arrayWays[0].amountNodes; i++) {
+        ui->textEdit->append(" " + arrayWays[0].nodes[i] + " ");
     }
 }
